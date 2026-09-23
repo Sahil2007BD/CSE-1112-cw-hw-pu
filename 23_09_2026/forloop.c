e0615369d4f1068e2d00220666f2836213d7e705
Sahil <sahiledu2007@gmail.com>
2026-09-23 12:28:22 +0600

    for loop program for c

---
+++ 23_09_2026/forloop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int i;
+    int n;
+    scanf("%d", &n);
+    for(i=1; i<=n; i++) {
+        printf("CSE_262_batch\n");
+    }
+
+
+
+
+
+return 0;
+
+}
